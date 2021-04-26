a7d5e79e53625cc08bbe0bb8ddd9233dbf80de9e
李宵逸 <13691419520@163.com>
2021-04-25 23:24:54 -0700

    db.py give up

--- Round1B/db.py
+++ Round1B/db.py
@@ -10,6 +10,7 @@ try:
 except Exception:
 	pass
 
+import random
 # import math, sys
 # sys.setrecursionlimit(100000000)
 from collections import defaultdict
@@ -29,17 +30,30 @@ for test in range(T):
 		h2i[h].add(n)
 	for i in range(N * B):
 		D = int(input())
-		if D >= 9 or (D >= 7 and i >= N * B * 0.8):
+		if D >= 9 or (D >= 8 and i >= N * B * 0.9):
 			place(next(iter(h2i[max(filter(lambda x: x != B, heights))])))
 #		elif D <= 1 or (D <= 4 and i >= N * B * 0.8):
 #			place(next(iter(h2i[min(filter(lambda x: x != B, heights))])))
 		elif not h2i[B - 1] or (i <= N * B * 0.8 and len(h2i[B - 1]) < 2):
 			place(next(iter(h2i[max(filter(lambda x: x < B - 1, heights))])))
 		else:
-			m = min(filter(lambda x: x != B, heights))
-			a = next(iter(h2i[m]))
+			ps = []
+			ws = []
+			for index, h in enumerate(heights):
+				if h == B:
+					continue
+				if h == B - 1:
+					w = 1
+				else:
+					w = 10000 / (abs(D * B - h * 10) + 1)
+				ps.append(index)
+				ws.append(w)
+			place(random.choices(ps, ws)[0])
+#			for i in filter(lambda x: x[1] < B, enumerate(heights))
+#			m = min(filter(lambda x: x != B, heights))
+#			a = next(iter(h2i[m]))
 #			print(m, a, heights[a], heights, h2i, file=sys.stderr)
-			place(a)
+#			place(a)
 	assert heights == [B] * N
 # 
 #   1000000000000000
@@ -49,4 +63,5 @@ print('               937467793908762347', file=sys.stderr)
 print(input(), file=sys.stderr)	# should be 1 on AC
 
 # 47 min, AC WA (estimated based on local testing)
+# 60 min, AC WA
 
